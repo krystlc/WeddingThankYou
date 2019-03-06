3759bde4fcba54d67761438cdbbaf38f8f7efc2b
Alex Valle <alex.valle@gmail.com>
2019-03-06 15:11:31 -0500

    ready

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core'
+import { trigger, state, style, animate, transition } from '@angular/animations'
+
+type Names = Array<{[index: number]: string}>
+type Vendors = Array<{type: string, name: string, url: string}>
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
+  animations: [
+    trigger('showHide', [
+      state('show', style({
+        opacity: 1,
+      })),
+      state('hide', style({
+        opacity: 0,
+        bottom: '-2em'
+      })),
+      transition('show => hide', [
+        animate('0.25s ease-out')
+      ]),
+      transition('hide => show', [
+        animate('0.25s ease-in-out')
+      ])
+    ])
+  ]
 })
+
 export class ContentComponent {
 
-  title = `Alex & Loren's Wedding Day`
+  constructor(public element: ElementRef) {
+    this.element.nativeElement
+  }
 
-  specialPeople = [
+  show: boolean = false
+
+  title: string = `Alex & Loren's Wedding Day`
+
+  specialPeople: Names = [
     'Jose Alberto Suarez Policarpio',
     'Arianne Trisha Ortezuela',
-    'Rhuster',
-    'Ken',
+    'Rhuster Bañez',
+    'Ken Go',
     'Batch 134',
     'Nicholas Valle',
     'Iraiza Valle',
@@ -31,7 +59,7 @@ export class ContentComponent {
     'Staff Family'
   ]
 
-  vendors = [
+  vendors: Vendors = [
     { type: 'Photographer', name: 'Cebu Wedding Photographer – Dennis Carpio', url: 'http://denniscarpiophotography.com/' },
     { type: 'Videographer', name: 'Joseph Bacalso Films & Stills', url: 'http://josephbacalso.com' },
     { type: 'Coordinator/Florist', name: 'Ruenas Events and Services', url: 'https://www.facebook.com/ruenaseventsandservice/' },
@@ -40,5 +68,26 @@ export class ContentComponent {
     { type: 'Videoke', name: 'Videoke For Rent Lapu-Lapu City', url: 'https://www.facebook.com/Videoke-For-Rent-Lapu-Lapu-City-280132819032505/' },
     { type: 'Venue', name: 'Mövenpick Hotel Mactan Island Cebu', url: 'https://www.facebook.com/movenpickcebu/' }
   ]
+
+  getNameList() {
+    return this.specialPeople.sort( () => Math.random() - 0.5) // randomize list
+  }
+
+  goTo(url: string) {
+    window.open(url, '_blank')
+  }
+
+  @HostListener("window:scroll", []) onWindowScroll() {
+    this.scrollFunction()
+  }
+
+  scrollFunction() {
+    const intro = this.element.nativeElement.querySelector('#intro').offsetHeight / 2 // show after half screen has been scrolled
+    if (document.body.scrollTop > intro || document.documentElement.scrollTop > intro) {
+      this.show = true
+    } else {
+      this.show = false
+    }
+  }
 
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,11 @@
 import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material'
+import {MatGridListModule} from '@angular/material/grid-list'
+import {MatRippleModule} from '@angular/material/core'
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatIconModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatIconModule]
+  imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatGridListModule, MatRippleModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatGridListModule, MatRippleModule]
 })
 
 export class MaterialModule { }
